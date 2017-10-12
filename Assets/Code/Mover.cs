fd595c8be08d616673f3611dc990383596f31d5b
Esa Nord <esa.nord@cs.tamk.fi>
2017-10-12 10:12:18 +0300

    Movement done

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,32 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+        [SerializeField, Tooltip ("Cube's speed")]
+        private float _Speed = 5f;
+
 		void Update()
 		{
+            Move();
 		}
-	}
+
+        private void Move()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * _Speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * _Speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * _Speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * _Speed * Time.deltaTime);
+            }
+        }
+    }
 }
