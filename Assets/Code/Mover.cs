900369f7cd7f1795649e20db85eec3206833c796
Valtteri Ojanen <valtteri.ojanen@cs.tamk.fi>
2017-10-12 10:10:44 +0300

    Jump implemented

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -6,6 +6,10 @@ namespace TAMK.VCSExample
 	{
 		void Update()
 		{
+            if(Physics.Raycast(transform.position,Vector3.down,0.5f) && Input.GetKey(KeyCode.Space))
+            {
+                GetComponent<Rigidbody>().AddForce(new Vector3(0, 40f, 0), ForceMode.Impulse);
+            }
 		}
 	}
 }
